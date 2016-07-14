2047ceab5052ca80b683c3f39488e4fa7e5ceb6b
plsaidan <hxzofficial@gmail.com>
2016-07-14 12:38:57 -0400

    Create 1.50_whileloops.js

---
+++ 1.50_whileloops.js
@@ -0,0 +1,11 @@
+//while loop loops through a block of code as long as a specified condition is true.
+// loops are used to repeatedly run a block of code
+var sum = 0;
+var number = 1;
+while (number <= 50) {  // condition
+  sum += number;        // body
+  number++;             // updater
+}
+alert("Sum = " + sum);  // => Sum = 1275
+
+//An alert box should pop up giving me the answer to my problem, 1275
