80d2975383b79867ba4299c7ac7ebf69110998b3
plsaidan <hxzofficial@gmail.com>
2016-07-14 13:25:41 -0400

    Create 1.71_objects.methods.js

---
+++ 1.71_objects.methods.js
@@ -0,0 +1,23 @@
+//object metods
+
+var rappers = {
+	//properties
+	name		: "Lil Boat",  //must have a comma after every property except the final one
+	age			: 18,
+	Hair-Color	: "Red like a Cherry",
+	keeps-them		: like a fairy,
+	SoundClould		: "Lil Yatchy,RD,Lil Boat",
+//you can add as many properties as you'd like
+	//method
+	Interview Answer	: function(){
+		console.log("You know its " + name + " its" name + " and lil perry, i keep them " + keeps-them + " my hair be " + Hair-Color)
+	},
+	listen		: function(){
+		console.log("You can listen to my mixtape on SoundCloud at " + SoundCloud);
+	}
+};
+
+rappers.InterviewAnswer(); //this will run: "You Know its lil boat its lil boat and lil perry i keep them like a fairy my hair be red like cherry
+rappers.listen(); //this will run: "You can listen to my mixtape on soundcloud at Lil Yachty,RD,Lil Boat
+
+//im listening to yachty sorry for the repetititveness
