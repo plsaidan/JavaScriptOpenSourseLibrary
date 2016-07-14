627619720e26034712f02e7d45fc81347f0b60ff
plsaidan <hxzofficial@gmail.com>
2016-07-14 11:49:19 -0400

    Create 1.60_arrays.js

---
+++ 1.60_arrays.js
@@ -0,0 +1,14 @@
+//Arrays
+
+//Arrays are to store a list or group of items
+//They can include strings, or integers, or both
+//they use square brackets []
+//The first entry in an array is positioned at 0
+
+var myDreamCars = ["Miata", "Masarati", "Lambo"]
+
+//Accessing items in an array
+
+console.log(myDreamCars); //will access the entire array 
+console.log(myDreamCars[0]); //will say Miata
+console.log(myDreamCars.length); //will tell me how long my array is, 3
