b689ca9e16751bf396903611086e7b8db6b41be5
plsaidan <hxzofficial@gmail.com>
2016-07-14 11:18:00 -0400

    Create 1.40_functions.js

---
+++ 1.40_functions.js
@@ -0,0 +1,18 @@
+function addnumbers(){
+	var x = 1
+	var y = 1
+	console.log(x = y)
+}
+
+function addTwoNumber(x, y){
+	console.log(x + y);
+}
+
+addTwoNumber(4, 5)
+addTwoNumber(6, 11)
+addTwoNumber(7, 8)
+
+function addMoreNumners(NumOne,  NumTwo){
+	console.log(NumOne + NumTwo)
+}
+
