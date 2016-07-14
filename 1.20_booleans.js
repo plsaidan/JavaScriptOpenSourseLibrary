afe9bce4bc8676691d147841a2c15a9fc0723b04
plsaidan <hxzofficial@gmail.com>
2016-07-14 12:48:02 -0400

    Create 1.20_booleans.js

---
+++ 1.20_booleans.js
@@ -0,0 +1,11 @@
+//A JavaScript boolean represents one of two values: true or false.
+
+7 > 4;                //runs as true
+1 > 10000;           //runs as false
+
+//******
+
+var deltaSwimTeamWins = 20;
+
+if (deltaSwimTeamWins > 19){         //runs console.log because it's true
+	console.log("20 wins and no losses for the third consecutive year!")
