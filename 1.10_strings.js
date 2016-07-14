89c05e87f2b5dacea5de073f78f4eac34b5fdb99
plsaidan <hxzofficial@gmail.com>
2016-07-14 11:03:28 -0400

    Create 1.10_strings.js

---
+++ 1.10_strings.js
@@ -0,0 +1,20 @@
+//Stings
+//"sike feral gang"
+
+var myName = "Aidan"
+console.log(myName)
+var password = "L!z4rd"
+var dob = " Feb 3"
+console.log(dob)
+console.log("Fa" + "Ze" + " Cl" + "an")
+
+var birthCity = "Muncie,"
+var birthState = " Indiana"
+
+var birthPlace = "Muncie," + " Indiana"
+console.log(birthPlace)
+
+var highschool = "Delta High School"
+var graduationYean = "2018"
+
+console.log("I will graduate from " + highschool + " in " + graduationYean)
