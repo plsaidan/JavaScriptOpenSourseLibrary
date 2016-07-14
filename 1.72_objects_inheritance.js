aeabb303a0f49d3ac3f3b9103997e45cf437d982
plsaidan <hxzofficial@gmail.com>
2016-07-14 13:39:11 -0400

    Create 1.72_objects_inheritance.js

---
+++ 1.72_objects_inheritance.js
@@ -0,0 +1,11 @@
+function Rapper(first, last, age, relevance) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.relevance = relevancer;
+}
+var lilYachty = new Rapper("Lil", "Boat", 18, " not much but, rising");
+var biggie = new Rapper("Notorious", "BIG", 43, "Massive");
+
+console.log("lilYachty") //this will run: Lil Boat, 18, not much but rising
+console.log("biggie") // this will run: Notorious BIG, 43, Massive
